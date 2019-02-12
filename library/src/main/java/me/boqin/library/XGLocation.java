fa889bfacad80b6c63788f0b1a77d11913b124d9
vito <boqin2x@163.com>
2019-02-12 16:59:40 +0800

    first commit

---
+++ library/src/main/java/me/boqin/library/XGLocation.java
@@ -0,0 +1,51 @@
+package me.boqin.library;
+
+
+import android.app.Activity;
+import android.content.Context;
+import android.util.Log;
+import me.boqin.library.utils.LocationUtils;
+import me.boqin.library.utils.SettingUtils;
+
+/**
+ * TODO
+ * Created by BoQin on 2018/3/1.
+ * Modified by BoQin
+ *
+ * @Version
+ */
+public class XGLocation {
+
+    public static void searchCurrentLocation(){
+
+    }
+
+    public boolean checkPermissions(Context context){
+        return LocationUtils.isGPSOpened(context);
+    }
+
+    public void goApplicationLocationSetting(Activity currentActivity){
+
+        if (currentActivity == null) {
+            Log.d("TFN", "Activity doesn't exist");
+            return;
+        }
+
+        SettingUtils.goApplicationLocationSetting(currentActivity);
+    }
+
+    public void goSystemLocationSetting(Activity currentActivity){
+//        if (null == mContext) {
+//            promise.reject(CONTEXT_NULL, "context is null");
+//            return;
+//        }
+//
+//
+//        if (currentActivity == null) {
+//            promise.reject(ACTIVITY_NOT_EXIST, "Activity doesn't exist");
+//            return;
+//        }
+
+        SettingUtils.goSystemLocationSetting(currentActivity);
+    }
+}
