a8849f51b60408e7a64dcef17d40317a2996cf82
vito <boqin2x@163.com>
2019-02-12 17:06:12 +0800

    sample 权限

--- app/src/main/java/me/boqin/rxamap/MainActivity.java
+++ app/src/main/java/me/boqin/rxamap/MainActivity.java
@@ -1,8 +1,11 @@
 package me.boqin.rxamap;
 
+import com.boqin.runtimepermissions.PermissionActivity;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+@PermissionActivity({})
 public class MainActivity extends AppCompatActivity {
 
     @Override
